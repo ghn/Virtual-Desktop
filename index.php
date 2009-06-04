f7cd5ddbf562f1a87b911831e1329d31e685d2f0
Germain <ghugon@gmail.com>
2009-06-04 11:44:50 +0200

    updates

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
-<?php
-	require_once('lib/starter.php');
-?>
+<?php require_once('lib/starter.php'); ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
     "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
